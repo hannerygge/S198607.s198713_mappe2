d2dd5c1da608fa936cdf242d5daef21e120cbddf
hannerygge <s198613@stud.hioa.no>
2015-10-19 14:53:01 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/hanne_000/s198607s198713_mappe2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.hanne_000.s198607s198713_mappe2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
