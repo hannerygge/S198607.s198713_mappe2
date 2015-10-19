aa61a640335f2d92ffc810bc73865c9681e03726
Proetic <datafriken_1@hotmail.com>
2015-10-19 15:58:06 +0200

    aekh

---
+++ app/src/main/java/com/example/hanne_000/s198607s198713_mappe2/Message.java
@@ -0,0 +1,48 @@
+package com.example.hanne_000.s198607s198713_mappe2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.DatePicker;
+
+public class Message extends Activity{
+
+    String Standardmessage = ""; //Must get this from database!
+    DatePicker dpResult;
+
+    public void updateStandardMessage(String message)
+    {
+        Standardmessage = message;
+        //update database
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message);
+        //Update standardmessage from db
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
