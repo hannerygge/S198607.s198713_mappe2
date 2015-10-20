6329347fbec66fde2eb75df124944f0a2a4f9226
hannerygge <s198613@stud.hioa.no>
2015-10-20 11:48:49 +0200

    fragment

---
+++ app/src/main/java/com/example/hanne_000/s198607s198713_mappe2/DatePickerFragment.java
@@ -0,0 +1,13 @@
+package com.example.hanne_000.s198607s198713_mappe2;
+
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+
+/**
+ * Created by hanne_000 on 20.10.2015.
+ */
+public class DatePickerFragment extends DialogFragment {
+
+
+
+}
---
+++ app/src/main/java/com/example/hanne_000/s198607s198713_mappe2/TimePickerFragment.java
@@ -0,0 +1,7 @@
+package com.example.hanne_000.s198607s198713_mappe2;
+
+/**
+ * Created by hanne_000 on 20.10.2015.
+ */
+public class TimePickerFragment {
+}
