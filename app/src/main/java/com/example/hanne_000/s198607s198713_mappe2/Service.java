85dba07866cf053ade17bd8714414adf1aeea362
hannerygge <s198613@stud.hioa.no>
2015-10-22 11:57:40 +0200

    service

--- app/src/main/java/com/example/hanne_000/s198607s198713_mappe2/Service.java
+++ app/src/main/java/com/example/hanne_000/s198607s198713_mappe2/Service.java
@@ -1,7 +1,32 @@
 package com.example.hanne_000.s198607s198713_mappe2;
 
 
-public class Service {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class Service extends android.app.Service{
+
+    String message;
+    String number;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
 
 
+        //Toast.makeText(getApplicationContext(),"I MinService", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+
+        //Bundle
+
+
+    }
 }
